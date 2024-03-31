4d4b391934f49add57c9fdad60886fb3aceb18eb
Lin Zhang <linroid@gmail.com>
2024-03-31 23:56:40 +0800

    Fix module not found for wasmJs

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -1,9 +1,11 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import com.linroid.pexels.App
+import com.linroid.pexels.Startup
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    Startup.run()
     CanvasBasedWindow("Pexels") {
         App()
     }
