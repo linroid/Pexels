5570d7bae5fdc416e430ded1e3e1649f52d1d2f0
Lin Zhang <linroid@gmail.com>
2024-04-01 15:43:06 +0800

    Implement loading more

--- composeApp/src/commonMain/kotlin/com/linroid/pexels/screen/curated/CuratedModel.kt
+++ composeApp/src/commonMain/kotlin/com/linroid/pexels/screen/curated/CuratedModel.kt
@@ -1,12 +1,9 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.linroid.pexels.screen.curated
 
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.pulltorefresh.PullToRefreshState
-import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import cafe.adriel.voyager.core.model.ScreenModel
 import com.linroid.pexels.api.PexelsApi
 import com.linroid.pexels.api.model.Photo
@@ -17,17 +14,18 @@ import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-class CuratedModel(
-	private val pullToRefreshState: PullToRefreshState
-) : ScreenModel, KoinComponent {
+class CuratedModel : ScreenModel, KoinComponent {
 
-	private var currentPage: Int = 1
-	private val api: PexelsApi by inject()
 	private val coroutineScope = MainScope()
-	val photos = mutableStateListOf<Photo>()
+	private val api: PexelsApi by inject()
+	private var currentPage: Int = 1
 
-	private var _isLoading = mutableStateOf(false)
-	val isLoading: State<Boolean> = _isLoading
+	private val photoIds = mutableSetOf<Int>()
+	private var _photos = mutableStateListOf<Photo>()
+	val photos: List<Photo> get() = _photos.toList()
+
+	private var _state by mutableStateOf(UiState.Idle)
+	val uiState: UiState get() = _state
 
 	init {
 		refresh()
@@ -39,20 +37,59 @@ class CuratedModel(
 	}
 
 	fun refresh() {
+		if (_state == UiState.Refreshing) {
+			return
+		}
 		Napier.i("refresh")
 		currentPage = 1
-		_isLoading.value = true
-		pullToRefreshState.startRefresh()
+		_state = UiState.Refreshing
 		coroutineScope.launch {
 			try {
 				val pagination = api.curatedPhotos(currentPage)
-				photos.clear()
-				photos.addAll(pagination.photos)
+				photoIds.clear()
+				_photos.clear()
+				_photos.addAll(pagination.photos)
+				photoIds.addAll(pagination.photos.map { it.id }.toSet())
+				_state = UiState.Idle
 			} catch (error: Exception) {
 				Napier.e("Unable to refresh", error)
-			} finally {
-				pullToRefreshState.endRefresh()
+				_state = UiState.Error
 			}
 		}
+	}
+
+	fun loadMore() {
+		if (_state == UiState.LoadingMore) {
+			return
+		}
+		currentPage++
+		Napier.i { "loading page $currentPage" }
+		_state = UiState.LoadingMore
+		coroutineScope.launch {
+			try {
+				val pagination = api.curatedPhotos(currentPage)
+				if (pagination.photos.isEmpty()) {
+					_state = UiState.End
+				} else {
+					/**
+					 * Pexels server may return duplicate photos, we need to filter them
+					 */
+					_photos.addAll(pagination.photos.filter { !photoIds.contains(it.id) })
+					photoIds.addAll(pagination.photos.map { it.id }.toSet())
+					_state = UiState.Idle
+				}
+			} catch (error: Exception) {
+				Napier.e("Unable to refresh", error)
+				_state = UiState.Error
+			}
+		}
+	}
+
+	enum class UiState {
+		Refreshing,
+		LoadingMore,
+		End,
+		Idle,
+		Error
 	}
 }
